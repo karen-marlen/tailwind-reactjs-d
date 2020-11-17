9aa4900bf7f8d8bbbaca4cf4037a23006776da34
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2020-11-16 23:05:56 -0300

    utils, d

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react';
 import {
   Switch,
-  Route,
-  useLocation
+  Route
 } from 'react-router-dom';
 
 import './styles/main.css';
 
-import AOS from 'aos';
-import { focusHandling } from 'cruip-js-toolkit';
 
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
@@ -16,24 +13,6 @@ import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
 
 function App() {
-
-  const location = useLocation();
-
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 700,
-      easing: 'ease-out-cubic',
-    });
-  });
-
-  useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
-    focusHandling('outline');
-  }, [location.pathname]); // triggered on route change
 
   return (
     <>
